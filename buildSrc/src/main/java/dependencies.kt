80a16e79ae5efbaa732322f3a4f96df0fef2cc24
IVIanuu <IVIanuu@gmail.com>
2019-01-25 18:16:26 +0100

    .

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -8,7 +8,7 @@ object Build {
     const val versionCode = 1
     const val versionName = "0.0.1"
 
-    const val groupId = "com.github.ivianuu.closeable"
+    const val groupId = "com.github.IVIanuu.closeable"
 }
 
 object Versions {

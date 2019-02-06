903a41aa47b61fed222ec34032e71f8b7dbf3770
IVIanuu <IVIanuu@gmail.com>
2019-02-06 01:47:48 +0100

    Moved sample package

--- sample/src/main/java/com/ivianuu/closeable/sample/MainActivity.kt
+++ sample/src/main/java/com/ivianuu/closeable/sample/MainActivity.kt
@@ -1,10 +1,29 @@
-package com.ivianuu.closeable
+/*
+ * Copyright 2018 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.closeable.sample
 
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.ivianuu.closeable.Closeable
+import com.ivianuu.closeable.CompositeClosable
+import com.ivianuu.closeable.R
 import com.ivianuu.closeable.coroutines.addTo
 import com.ivianuu.closeable.rx.addTo
 import io.reactivex.Observable
